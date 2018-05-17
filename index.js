85526d43b84cb64fd582007d5acb53e89049a12f
eduardosanzbOptilyz <eduardo.sanchez@optilyz.com>
2018-05-17 20:31:14 +0200

    ✨ Added shebang

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
-}
+#!/usr/bin/env node
 
-(() => {
-  console.log('hello');
-})()
+'use strict';
+
+console.log('Hello');
