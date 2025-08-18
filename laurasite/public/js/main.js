6b336dbe07abfea6411df52291fb79f548e5e365
Eduardo Bautista <eduardosanzb@gmail.com>
2025-08-18 21:49:26 +0200

    changes

--- laurasite/public/js/main.js
+++ laurasite/public/js/main.js
@@ -2,13 +2,13 @@
 
 let scriptBody;
 function cally(C, A, L) {
-  let p = function(a, ar) {
+  let p = function (a, ar) {
     a.q.push(ar);
   };
   let d = C.document;
   C.Cal =
     C.Cal ||
-    function() {
+    function () {
       let cal = C.Cal;
       let ar = arguments;
       if (!cal.loaded) {
@@ -19,7 +19,7 @@ function cally(C, A, L) {
         cal.loaded = true;
       }
       if (ar[0] === L) {
-        const api = function() {
+        const api = function () {
           p(api, arguments);
         };
         const namespace = ar[1];
@@ -69,13 +69,11 @@ function refreshCal() {
   initCal();
 }
 
-document
-  .getElementById("theme-toggle")
-  .addEventListener("click", refreshCal);
+document.getElementById("theme-toggle").addEventListener("click", refreshCal);
 
 addEventListener("storage", refreshCal);
 
 matchMedia("(prefers-color-scheme: dark)").addEventListener(
   "change",
-  refreshCal
+  refreshCal,
 );
